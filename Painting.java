c74909574161a55e36692aab0234bb6dcf69d859
bretrandallmeyer <bretlorica09@gmail.com>
2019-01-15 12:18:33 +0800

    PaintingComponents:DICK SHAPED attempt

--- Painting.java
+++ Painting.java
@@ -6,16 +6,23 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		g.setColor(Color.GREEN);
+		g.fillRoundRect(400, 200, 200, 200, 100, 100);
+
+		g.setColor(Color.BLUE);
+		g.fillRect(50, 250, 50, 100);
+
+		g.setColor(Color.GREEN);
+		g.fillOval(90, 50, 50,100);
 	}
 
 	public static void main(String args[]){
 		JFrame myFrame = new JFrame("Painting");
-		// JPanel panel1 = new JPanel();
+		//JPanel panel1 = new JPanel();
 
 		Painting myPainting = new Painting();
 
-		// panel1.setBackground(Color.GREEN);
+		//panel1.setBackground(Color.GREEN);
 
 		myFrame.add(myPainting);
 
