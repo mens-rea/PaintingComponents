9ee342d940c5d05b10f3fa74e10e5a078b19ce4a
lowe25 <louiealdrin25@gmail.com>
2019-01-15 11:47:23 +0800

    Louie Cabral

--- Painting.java
+++ Painting.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.JLabel;
 
 public class Painting extends JPanel{
 
@@ -7,18 +8,33 @@ public class Painting extends JPanel{
 		super.paintComponent(g);
 
 		g.fillOval(0, 250, 50, 100);
+		g.fillOval(0, 300, 60, 500);
+		g.fillOval(20, 300, 60, 500);
+		g.fillOval(50, 300, 60, 500);
+		g.setColor(Color.YELLOW);
+
+		g.fillRect(100,200,200,200);
+
+
 	}
+
+
 
 	public static void main(String args[]){
 		JFrame myFrame = new JFrame("Painting");
-		// JPanel panel1 = new JPanel();
-
+		JPanel panel1 = new JPanel();
+         JLabel label = new JLabel("LOUIE ALDRIN CABRAL");
+         label.setVisible(true);
+         label.setSize(500,100);
+         myFrame.setResizable(false);
+         label.setBounds(100,200,200,200);
 		Painting myPainting = new Painting();
 
-		// panel1.setBackground(Color.GREEN);
+		panel1.setBackground(Color.GREEN);
 
+          
+  myFrame.add(label);
 		myFrame.add(myPainting);
-
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setSize(700, 500);
 		myFrame.setVisible(true);
