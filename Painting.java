1b7efdad23451dab9d77d8c379ffa99bce7d84dd
Maye-Day <45581328+Maye-Day@users.noreply.github.com>
2019-01-15 13:53:59 +0800

    This is first Commit

--- Painting.java
+++ Painting.java
@@ -8,13 +8,9 @@ public class Painting extends JPanel{
 		super.paintComponent(g);
 
 		g.fillOval(0, 250, 50, 100);
-		g.fillOval(0, 300, 60, 500);
-		g.fillOval(20, 300, 60, 500);
-		g.fillOval(50, 300, 60, 500);
-		g.setColor(Color.YELLOW);
+		g.fillRect(20, 300, 60, 500);
+		g.setColor(Color.ORANGE);
 
-		g.fillRect(100,200,200,200);
-		g.fillRect(500,200,300,400);
 	
 
 
@@ -25,16 +21,16 @@ public class Painting extends JPanel{
 	public static void main(String args[]){
 		JFrame myFrame = new JFrame("Painting");
 		JPanel panel1 = new JPanel();
-         JLabel label = new JLabel("LOUIE ALDRIN CABRAL");
+         JLabel label = new JLabel("James Allen Maye III");
          label.setVisible(true);
          label.setSize(500,100);
          myFrame.setResizable(false);
          label.setBounds(100,200,200,200);
 		Painting myPainting = new Painting();
 
-		panel1.setBackground(Color.GREEN);
+		panel1.setBackground(Color.BLUE);
 
-          
+
   myFrame.add(label);
 		myFrame.add(myPainting);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
