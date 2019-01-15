7f4fb26f159fec776821c83e831fdf2043df7a70
chubzielex <reyes.lexus@gmail.com>
2019-01-15 11:30:39 +0800

    edited version of your file

--- Painting.java
+++ Painting.java
@@ -6,7 +6,21 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
+		g.setColor(Color.BLACK);
 		g.fillOval(0, 250, 50, 100);
+
+		g.setColor(Color.RED);
+		g.fillRect(50, 250, 50, 100);
+
+		g.setColor(Color.WHITE);
+		g.fillRoundRect(100, 250, 250, 100, 100, 100);
+		
+
+	int xpoints[] = {25, 145, 25, 145, 25};
+    int ypoints[] = {25, 25, 145, 145, 25};
+    int npoints = 5;
+    	g.setColor(Color.GREEN);
+    	g.fillPolygon(xpoints, ypoints, npoints);
 	}
 
 	public static void main(String args[]){
@@ -15,7 +29,7 @@ public class Painting extends JPanel{
 
 		Painting myPainting = new Painting();
 
-		// panel1.setBackground(Color.GREEN);
+		myPainting.setBackground(Color.BLUE);
 
 		myFrame.add(myPainting);
 
