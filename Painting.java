34b56af60a7b1fb06a808c87bc7877a6a92f936c
Pooooong <kyilomolon1714@gmail.com>
2019-01-15 12:02:35 +0800

    IncrediblesLogo

--- Painting.java
+++ Painting.java
@@ -6,7 +6,23 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+
+		g.setColor(Color.ORANGE);
+		g.fillOval(315, 60, 1000, 1000);
+
+		g.setColor(Color.BLACK);
+		g.fillOval(470, 200, 700, 700);
+
+		g.setColor(Color.YELLOW);
+		g.fillOval(700, 250, 250, 250);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(750, 300, 150, 150);
+		
+		g.setColor(Color.YELLOW);
+		g.fillRect(750, 500, 150, 300);
+
+
 	}
 
 	public static void main(String args[]){
@@ -15,12 +31,14 @@ public class Painting extends JPanel{
 
 		Painting myPainting = new Painting();
 
+		myPainting.setBackground(Color.PINK);
+
 		// panel1.setBackground(Color.GREEN);
 
 		myFrame.add(myPainting);
 
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.setSize(700, 500);
+		myFrame.setSize(1700, 1500);
 		myFrame.setVisible(true);
 	}
 }
