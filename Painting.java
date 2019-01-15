4e442a288627625a9695c153dd81e7d0b92dab04
Brentyy <brentrivera282@gmail.com>
2019-01-15 12:04:00 +0800

    MyPainting

--- Painting.java
+++ Painting.java
@@ -6,21 +6,28 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		g.setColor(Color.GREEN);
+		g.fillOval(700, 250, 250, 200);
+		g.setColor(Color.BLUE);
+		g.fillRect(100, 150, 150, 100);
+		g.setColor(Color.BLACK);
+		g.fillOval(200, 250, 250, 100);
+
+
 	}
 
 	public static void main(String args[]){
 		JFrame myFrame = new JFrame("Painting");
-		// JPanel panel1 = new JPanel();
+		//JPanel panel1 = new JPanel();
 
 		Painting myPainting = new Painting();
 
-		// panel1.setBackground(Color.GREEN);
+		//panel1.setBackground(Color.GREEN);
 
 		myFrame.add(myPainting);
 
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.setSize(700, 500);
+		myFrame.setSize(600, 550);
 		myFrame.setVisible(true);
 	}
 }
