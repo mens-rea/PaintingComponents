ca97c126e82a7facd6e0ec0038eeea9b016a4eb7
deathzky1 <khern02@yahoo.com>
2019-01-15 14:48:17 +0800

    change background

--- Painting.java
+++ Painting.java
@@ -6,7 +6,21 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		g.setColor(Color.BLUE);
+		g.fillOval(20, 20, 100, 100);
+		g.setColor(Color.MAGENTA);
+		g.fillRect(100,100,100,100);
+		g.setColor(Color.GREEN);
+		g.fillRoundRect(200,200,200,200,200,200);
+
+		g.setColor(Color.BLACK);
+		int [] x = {50,50,90,90,150,90,90};
+        int [] y = {55,85,85,110,70,30,55};
+        g.fillPolygon(x, y, 7);
+
+ 
+		
+
 	}
 
 	public static void main(String args[]){
