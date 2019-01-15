68fb67dda6bff66dda06cdda1b888fd668698ab0
hustinaaa <jraeviel@yahoo.com>
2019-01-15 12:05:33 +0800

    edited

--- Painting.java
+++ Painting.java
@@ -1,44 +1,37 @@
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.JLabel;
 
 public class Painting extends JPanel{
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
-		g.fillOval(0, 300, 60, 500);
-		g.fillOval(20, 300, 60, 500);
-		g.fillOval(50, 300, 60, 500);
-		g.setColor(Color.YELLOW);
-
-		g.fillRect(100,200,200,200);
-		g.fillRect(500,200,300,400);
-	
-
-
+		g.setColor(Color.black);
+		g.fillRect(275, 250, 275, 250);
+		g.setColor(Color.pink);
+		g.fillOval(100, 150, 100, 150);
+		g.setColor(Color.blue);
+		g.fillRoundRect(250, 120, 170, 160, 125, 125);
+		g.setColor(Color.white);
+		g.fillArc(120, 130, 110, 150, 15, 150);
+		
+		
 	}
-
-
 
 	public static void main(String args[]){
 		JFrame myFrame = new JFrame("Painting");
-		JPanel panel1 = new JPanel();
-         JLabel label = new JLabel("LOUIE ALDRIN CABRAL");
-         label.setVisible(true);
-         label.setSize(500,100);
-         myFrame.setResizable(false);
-         label.setBounds(100,200,200,200);
+		// JPanel panel1 = new JPanel();
+
 		Painting myPainting = new Painting();
 
-		panel1.setBackground(Color.GREEN);
+		myPainting.setBackground(Color.yellow);
 
-          
-  myFrame.add(label);
+		// panel1.setBackground(Color.GREEN);
+
 		myFrame.add(myPainting);
+
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setSize(700, 500);
 		myFrame.setVisible(true);
 	}
-}
+} 
