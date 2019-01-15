7092643db4fca63c9624714a3562e9bf2c75111e
marinellaom <moral1820393@mkt.ceu.edu.ph>
2019-01-15 12:19:22 +0800

    add shapes

--- Painting.java
+++ Painting.java
@@ -1,12 +1,27 @@
 import java.awt.*;
 import javax.swing.*;
 
+
 public class Painting extends JPanel{
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		Graphics2D g2 = (Graphics2D) g;
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(30, 180, 100, 100);
+		g.drawOval(25, 175, 110, 110);
+
+		g.setColor(Color.BLACK);
+		g.fillRect(30, 30, 101, 101);
+		g.drawRect(25, 25, 111, 111);
+
+		g.setColor(Color.BLACK);
+		g.fillRect(30, 320, 101, 101);
+		g.drawRect(25, 315, 111, 111);
+
+		
 	}
 
 	public static void main(String args[]){
