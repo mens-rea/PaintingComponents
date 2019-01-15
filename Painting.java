878ba9dac7707ea9b5ea53f234cb6d1b93646f39
MimiPark06 <dumawal1820241@mkt.ceu.edu.ph>
2019-01-15 13:57:07 +0800

    Painting Dumawal

--- Painting.java
+++ Painting.java
@@ -6,7 +6,20 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		g.fillRect(0, 0, 700, 400);
+		g.setColor(Color.YELLOW);
+		g.fillOval(5, 5, 120, 120);
+		g.setColor(Color.PINK);
+		g.fillRect(50,250,250,150);
+		g.setColor(Color.RED);
+		g.drawLine(50, 250, 175, 50);
+		g.drawLine(300, 250, 175, 50);
+		g.setColor(Color.BLACK);
+		g.fillRect(400,300,10,100);
+		g.setColor(Color.GREEN);
+		g.fillOval(365, 230, 80, 80);
+		g.setColor(Color.GREEN);
+		g.fillRect(0, 400, 700, 350);
 	}
 
 	public static void main(String args[]){
