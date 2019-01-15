759d7ca362b725c26c1fd9dc75ba3239ad981633
ArtificialAwakening360 <jrom2899@gmail.com>
2019-01-15 13:35:05 +0800

    Insert Picture

--- Painting.java
+++ Painting.java
@@ -6,21 +6,36 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		g.setColor(Color.YELLOW);
+		g.fillOval(500, 50, 100, 100);
+		
+		g.setColor(Color.ORANGE);
+		g.fillRect(100,300,150,100);
+
+		g.setColor(Color.GREEN);
+		g.fillRect(125,250,100,50);
+
+		g.setColor(Color.BLACK);
+		g.fillRect(225,260,50,25);
+
+		g.setColor(Color.BLACK);
+		g.fillRect(150,240,50,10);
+
+		g.setColor(Color.GREEN);
+		g.fillRect(0,400,700,350);
 	}
 
 	public static void main(String args[]){
 		JFrame myFrame = new JFrame("Painting");
-		// JPanel panel1 = new JPanel();
-
+		
 		Painting myPainting = new Painting();
-
-		// panel1.setBackground(Color.GREEN);
-
+		
 		myFrame.add(myPainting);
-
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setSize(700, 500);
 		myFrame.setVisible(true);
+		myFrame.setResizable(false);
+		myFrame.setLocationRelativeTo(null);
+
 	}
 }
