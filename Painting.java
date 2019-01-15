e3c8dc6b4a215da03b0f20282028940a5f6494df
jajajajaci <jajajajaci>
2019-01-15 12:07:31 +0800

    CaptainAmerica

--- Painting.java
+++ Painting.java
@@ -6,7 +6,21 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+
+
+		g.setColor(Color.RED);
+		g.fillOval(50, 50, 600, 600);
+
+		g.setColor(Color.WHITE);
+		g.fillOval(100, 100, 500, 500);
+
+		g.setColor(Color.RED);
+		g.fillOval(150, 150, 400, 400);
+
+		g.setColor(Color.BLUE);
+		g.fillOval(200, 200, 300, 300);
+
+
 	}
 
 	public static void main(String args[]){
@@ -15,12 +29,14 @@ public class Painting extends JPanel{
 
 		Painting myPainting = new Painting();
 
+		myPainting.setBackground(Color.GRAY);
+
 		// panel1.setBackground(Color.GREEN);
 
 		myFrame.add(myPainting);
 
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.setSize(700, 500);
+		myFrame.setSize(1000, 1000);
 		myFrame.setVisible(true);
 	}
 }
