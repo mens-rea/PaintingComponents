b895b0bf0c6de37db91c7f7d704527ff33a9999e
ojamskie <plasabas1820495@mkt.ceu.edu.ph>
2019-01-15 11:42:00 +0800

    Editing your old file

--- Painting.java
+++ Painting.java
@@ -6,16 +6,30 @@ public class Painting extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		g.setColor(Color.RED);
+		g.fillOval(250, 250, 75, 75);
+		g.setColor(Color.BLUE);
+		g.fillRect(300, 200, 100, 100);
+		g.setColor(Color.BLACK);
+		g.fillOval(100, 100, 25, 25);
+		g.setColor(Color.GREEN);
+		g.fillRoundRect(400, 200, 200, 200, 100, 100);
+		g.setColor(Color.PINK);
+		g.fillRect(300, 50, 46, 130);
+		g.setColor(Color.PINK);
+		g.fillRoundRect(200, 250, 110, 50, 56, 45);
+		g.setColor(Color.BLACK);
+		g.fillArc(150, 350, 95, 45, 150, 200);
 	}
 
 	public static void main(String args[]){
 		JFrame myFrame = new JFrame("Painting");
-		// JPanel panel1 = new JPanel();
+		//JPanel Panel1 = new JPanel();
 
 		Painting myPainting = new Painting();
+		//Panel1.setBackground(Color.RED);
 
-		// panel1.setBackground(Color.GREEN);
+		myPainting.setBackground(Color.YELLOW);
 
 		myFrame.add(myPainting);
 
