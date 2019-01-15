fd8ea28ce1e590bf1fe61a0084c83fc7841cf7a5
LorLynnee <germino1820308@mkt.ceu.edu.ph>
2019-01-15 14:29:27 +0800

    fix branch

--- Painting.java
+++ Painting.java
@@ -1,21 +1,54 @@
 import java.awt.*;
 import javax.swing.*;
 
+
 public class Painting extends JPanel{
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		g.setColor(Color.BLACK);  
+		g.fillRect(100, 100, 100, 100);
+
+		g.setColor(Color.PINK);
+		g.drawOval(100, 100, 100, 100);
+		g.fillOval(105, 105, 90, 90);
+
+		g.setColor(Color.BLACK);  
+		g.fillRect(200, 200, 100, 100);
+
+		g.setColor(Color.PINK);
+		g.drawOval(200, 200, 100, 100);
+		g.fillOval(205, 205, 90, 90);
+
+		g.setColor(Color.BLACK);  
+		g.fillRect(300, 300, 100, 100);
+
+		g.setColor(Color.PINK);
+		g.drawOval(300, 300, 100, 100);
+		g.fillOval(305, 305, 90, 90);
+
+		g.setColor(Color.BLACK);  
+		g.fillRect(400, 400, 100, 100);
+
+		g.setColor(Color.PINK);
+		g.drawOval(400, 400, 100, 100);
+		g.fillOval(405, 405, 90, 90);
+
+
+
 	}
 
-	public static void main(String args[]){
+	public static void main(String[] args) {
+
+
 		JFrame myFrame = new JFrame("Painting");
-		// JPanel panel1 = new JPanel();
+		
+		//JPanel panel1 = new JPanel();
 
 		Painting myPainting = new Painting();
 
-		// panel1.setBackground(Color.GREEN);
+		//panel1.setBackground(Color.GREEN);
 
 		myFrame.add(myPainting);
 
