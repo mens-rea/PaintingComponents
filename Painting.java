77ecbda07ce8a5037f9d75cb894c616577687e6b
Kurtpogixx <Kurtjonleyvergeldedios@gmail.com>
2019-01-15 12:10:18 +0800

    MyShapes

--- Painting.java
+++ Painting.java
@@ -4,23 +4,36 @@ import javax.swing.*;
 public class Painting extends JPanel{
 
 	public void paintComponent(Graphics g){
-		super.paintComponent(g);
 
-		g.fillOval(0, 250, 50, 100);
+		super.paintComponent(g);
+		g.setColor(Color.PINK);
+		g.fillOval(650, 250, 100, 100);
+		g.setColor(Color.PINK);
+		g.fillRect(500, 250, 100, 100);
+
 	}
+
+
+
 
 	public static void main(String args[]){
-		JFrame myFrame = new JFrame("Painting");
-		// JPanel panel1 = new JPanel();
+		
+        JFrame myFrame = new JFrame("Painting");
+         //JPanel panel1 = new JPanel();
 
-		Painting myPainting = new Painting();
+        Painting myPainting = new Painting();
 
-		// panel1.setBackground(Color.GREEN);
+        myPainting.setBackground(Color.BLACK);
 
-		myFrame.add(myPainting);
+         //panel1.setBackground(Color.BLACK);
 
-		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.setSize(700, 500);
-		myFrame.setVisible(true);
-	}
-}
+        myFrame.add(myPainting);
+
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(700, 500);
+        myFrame.setVisible(true);
+
+
+
+        }
+    }
